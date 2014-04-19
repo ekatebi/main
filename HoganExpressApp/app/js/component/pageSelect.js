c7e802bd90da7a533ab8c2db66232ca9722b84de
Ed Katebi <ekatebi@gmail.com>
2014-04-19 14:17:34 -0400

    new comment

--- HoganExpressApp/app/js/component/pageSelect.js
+++ HoganExpressApp/app/js/component/pageSelect.js
@@ -77,6 +77,8 @@ define(function (require) {
               var li = $(e.target).closest('li');
               this.ActivateLi(li);
 
+              //comment
+              
               if (li.is(this.select('pageNavBarProf'))) {
                   profile.attachTo('#profilePage');
               }
